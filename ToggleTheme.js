773feb735a82b95b36e3dd697da41210fc23bcd1
Jordan Joseph <jordantaylor@Jordans-MacBook-Pro.local>
2025-05-26 16:36:54 -0600

    add pets info & themes

--- ToggleTheme.js
+++ ToggleTheme.js
@@ -9,9 +9,12 @@ import { View, ScrollView, Image } from 'react-native';
 const ToggleTheme = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-
+  const changeTheme = () => {
+    setIsDarkTheme((prevTheme) => !prevTheme);
+  };
 
   return (
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <Container>
         <ScrollView contentContainerStyle={{ paddingBottom: 20 }}> 
           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 20 }}> {/* Add marginTop to avoid first heading being cut off */}
@@ -19,10 +22,58 @@ const ToggleTheme = () => {
           </View>
 
          {/* Define Pet Care Section */}
+          <View testID='catCareTips'>
+            <Image
+              source={{ uri: 'https://images.unsplash.com/photo-1482434368596-fbd06cae4f89?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+              style={{ width: '100%', height: 400 }}
+            />
+            <ContentSection>
+              <SectionTitle>Cat Care Tips</SectionTitle>
+              <ContentText>
+                - Litter Box Hygiene: Keep the litter box clean by scooping daily and changing the litter frequently.{'\n'}
+                - Scratching Posts: Provide scratching posts to prevent furniture damage.{'\n'}
+                - Grooming: Regularly brush your cat to reduce shedding and prevent hairballs.{'\n'}
+                - Safe Spaces: Give cats cozy, quiet spots to relax.{'\n'}
+                - Playtime: Use interactive toys to keep them mentally stimulated.
+              </ContentText>
+            </ContentSection>
+          </View>
 
-        </ScrollView>
-        
-      </Container>
+          <View testID='dogCareTips'>
+            <ContentSection>
+                <SectionTitle>Dog Care Tips</SectionTitle>
+                  <ContentText>
+                    - Potty Time: Take your puppy outside regularly.{'\n'}
+                    - Grooming: Regularly brush your dog to reduce shedding.{'\n'}
+                    - Playtime: Play fetch!
+                  </ContentText>
+              </ContentSection>
+          </View>
+
+          <View testID='rabbitCareTips'>
+            <ContentSection>
+                <SectionTitle>Rabbit Care Tips</SectionTitle>
+              </ContentSection>
+          </View>
+
+          <View testID='fishCareTips'>
+            <ContentSection>
+                <SectionTitle>Fish Care Tips</SectionTitle>
+              </ContentSection>
+          </View>
+
+          <View testID='birdCareTips'>
+            <ContentSection>
+                <SectionTitle>Bird Care Tips</SectionTitle>
+              </ContentSection>
+          </View>
+          <StyledButton onPress={changeTheme}>
+            <ButtonText>{isDarkTheme ? 'Light Mode' : 'Dark Mode'}</ButtonText>
+          </StyledButton>
+
+          </ScrollView>
+        </Container>
+        </ThemeProvider>
   );
 };
 
@@ -38,5 +89,53 @@ const StyledText = styled.Text`
   font-style:italic;
   font-weight: bold;
 `;
+
+const ContentSection = styled.View`
+  margin-bottom: 20px;
+  padding: 10px;
+`;
+
+const SectionTitle = styled.Text`
+  font-size: 20px;
+  color: ${(props) => props.theme.colors.text};
+  font-weight: bold;
+`;
+
+const ContentText = styled.Text`
+  font-size: 16px;
+  color: ${(props) => props.theme.colors.text};
+`;
+
+const StyledButton = styled.TouchableOpacity`
+  background-color: ${(props) => props.theme.colors.buttonBackground};
+  padding: 10px 20px;
+  border-radius: 5px;
+  align-items: center;
+  margin-top: 10px;
+`;
+
+const ButtonText = styled.Text`
+  color: ${(props) => props.theme.colors.buttonText};
+  font-size: 23px;
+  font-weight: bold;
+`;
+
+const lightTheme = {
+  colors: {
+    background: '#ffffff',
+    buttonBackground: '#000000',
+    buttonText: '#ffffff',
+    text: '#000000',
+  },
+};
+
+const darkTheme = {
+  colors: {
+    background: '#282828',
+    buttonBackground: '#FF6347',
+    buttonText: '#282828',
+    text: '#FFD700',
+  },
+};
 
 export default ToggleTheme;
